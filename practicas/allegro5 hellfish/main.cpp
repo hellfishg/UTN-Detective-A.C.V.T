deed9a7f3fdb6b62557206ea0261a14d983632c8
hellfishg <hellfish.g@protonmail.com>
2016-10-31 22:30:36 -0300

    Codigo de inicio de las clases allegro.

--- practicas/allegro5 hellfish/main.cpp
+++ practicas/allegro5 hellfish/main.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <allegro5/allegro5.h>
-
+#include <allegro5/allegro_native_dialog.h>//Carga ventanas nativas del sistama
 using namespace std;
 
 ALLEGRO_DISPLAY *display;
 
 int main(void) {
 
-    al_init();
 
-    display=al_create_display(800,600);
 
-    al_clear_to_color(al_map_rgb(255,0,0));
+   ALLEGRO_DISPLAY *display;//Se crea el puntero display.
 
-    al_flip_display();
+   if(!al_init()){
+      al_show_native_message_box (NULL,"Titulo","error","Display window could be show","Botton1",ALLEGRO_MESSAGEBOX_ERROR);
 
-    al_rest(5);
+   } //Carga los principios de allegro5.Como el constructor.
 
-    al_destroy_display(display);
+   display = al_create_display (800,600);//el tamaño de la ventana.
+
+   if (!display) {//se instancia el display y muestra.
+      al_show_native_message_box (display, "Titulo","Settings","Ventana",NULL,ALLEGRO_MESSAGEBOX_ERROR);
+
+   }
+
+    al_show_native_message_box (display,"Titulo","errorm error","Error del semental","Botton1",ALLEGRO_MESSAGEBOX_ERROR);
+    al_show_native_message_box (display,"Titulo","errorm error","Error del semental","Botton1",ALLEGRO_MESSAGEBOX_WARN);
+    al_show_native_message_box (display,"Titulo","errorm error","Error del semental","Botton1",ALLEGRO_MESSAGEBOX_QUESTION);
+    al_show_native_message_box (display,"Titulo","errorm error","Error del semental","Botton1",ALLEGRO_MESSAGEBOX_OK_CANCEL);
+    al_show_native_message_box (display,"Titulo","errorm error","Error del semental","Botton1",ALLEGRO_MESSAGEBOX_YES_NO);
+   //  al_clear_to_color(al_map_rgb(255,0,0));
+   //  al_flip_display();
+
+     al_rest(1); //cierra el display.
+
+
+     al_destroy_display(display);//regresa la memoria usada.
 
     return 0;
 }
